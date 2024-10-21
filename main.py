cd23ee2673d8eb8e9eb97f45a5bc026bab28fe2a
Alex <noreply@example.com>
2024-10-21 23:29:39 +0300

    created bot (alpha 0.1)

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import requests
+import asyncio
+import logging
+
 from bs4 import BeautifulSoup
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+
+logging.basicConfig(level=logging.INFO)
+
+BOT_TOKEN = '7527348944:AAHPqtbr7qAFPGoSVf5w9vdYEPDYb5ZYX-8'
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
 
 
 def get_timetable(link):
+    res = ''
     response = requests.get(link).text
     soup = BeautifulSoup(response, 'lxml')
     for day in soup.select('.schedule__day'):
         date = day.select('.schedule__date')[0].text
-        print(date)
+        res += date + '\n\n'
         for lesson in day.select('.lesson'):
             start_time = lesson.select('.lesson__time')[0].find_all('span')[0].text
             end_time = lesson.select('.lesson__time')[0].find_all('span')[2].text
@@ -20,14 +34,29 @@ def get_timetable(link):
                 teacher = ''
             place = lesson.select('.lesson__places')[0].find_all('span')
             place = f'{place[0].text.strip()} {place[6].text.strip()} {place[7].text}'
-            print(f'{start_time} - {end_time} {lesson_subject} ({lesson_type.lower()})')
-            print(f'              {place}')
-            print(f'              {teacher}')
+            res += (f'{start_time} - {end_time} {lesson_subject} ({lesson_type.lower()})\n\n'
+                    f'{place}\n\n'
+                    f'{teacher}\n\n')
 
 
-        print('\n')
+        res += '\n\n'
+    return res
+
+
+@dp.message(Command('start'))
+async def start_command(message: types.Message):
+    await message.answer('Введи команду /timetable, чтобы получить расписание на неделю')
+
+
+@dp.message(Command('timetable'))
+async def print_timetable(message: types.Message):
+    link = 'https://ruz.spbstu.ru/faculty/125/groups/40399'
+    await message.answer(get_timetable(link))
+
+
+async def main():
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    link = 'https://ruz.spbstu.ru/faculty/125/groups/40399'
-    get_timetable(link)
+    asyncio.run(main())

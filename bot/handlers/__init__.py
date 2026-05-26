ea46c32b201f077a44cdc694d7c779592c8d0acf
alex <alex.lopatin456@gmail.com>
2026-05-26 19:52:16 +0300

    fix: updated gitignore and removed cache

--- bot/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router
-
-from .base import router as base_router
-from .timetable import router as timetable_router
-
-router = Router(name="main_router")
-router.include_routers(base_router, timetable_router)

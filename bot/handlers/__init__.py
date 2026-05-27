f076214b2d810965a805a56272467621320a27be
alex <alex.lopatin456@gmail.com>
2026-05-27 18:05:41 +0300

    updated .gitignore

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,7 +1,8 @@
 from aiogram import Router
 
 from .base import router as base_router
+from .groups import router as groups_router
 from .timetable import router as timetable_router
 
 router = Router(name="main_router")
-router.include_routers(base_router, timetable_router)
+router.include_routers(base_router, groups_router, timetable_router)
